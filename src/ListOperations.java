6b79b6acaf777ac30637c669a19c595f397c648f
ErikSai <aaron.howe99@gmail.com>
2023-03-10 16:52:55 -0800

    Added comments estimating the time complexity of each method to the ListOperations class, and added LinkedList test cases to main.

--- src/ListOperations.java
+++ src/ListOperations.java
@@ -6,11 +6,15 @@ public class ListOperations {
      * @param nums The list of integers
      * @return the sum of the values
      */
+    // O(2N)
     public int sum_getImple(java.util.List<Integer> nums) {
+        // 1
         int total = 0;
+        // N
         for(int i = 0; i < nums.size(); i++) {
             total += nums.get(i);
         }
+        // 1
         return total;
     }
 
@@ -19,12 +23,16 @@ public class ListOperations {
      * @param nums The list of integers
      * @return The sum of the values
      */
+    // O(2N)
     public int sum_iteratorImple(java.util.List<Integer> nums) {
+        // 1
         int total = 0;
         java.util.Iterator<Integer> it = nums.iterator();
+        // O(N)
         while(it.hasNext()) {
             total += it.next();
         }
+        // 1
         return total;
     }
 
@@ -33,8 +41,11 @@ public class ListOperations {
      * The method modifies the parameter list.
      * @param nums The list of integers
      */
+    // O(N^2)
     public void removeEvens_getImple(java.util.List<Integer> nums) {
+        // O(N)
         for(int i = 0; i < nums.size(); i++) {
+            // O(N)
             if(nums.get(i) % 2 == 0) {
                 nums.remove(i);
             }
@@ -46,9 +57,12 @@ public class ListOperations {
      * The method modifies the parameter list.
      * @param nums The list of integers
      */
+    // O(3N)
     public void removeEvens_iteratorImple(java.util.List<Integer> nums) {
         java.util.Iterator<Integer> it = nums.iterator();
+        // O(N)
         while(it.hasNext()) {
+            // 3
             int value = it.next();
             if(value % 2 == 0) {
                 it.remove();
@@ -61,8 +75,11 @@ public class ListOperations {
      * This method modifies the parameter list.
      * @param nums The list of integers
      */
+    // O(2N)
     public void double_getImple(java.util.List<Integer> nums) {
+        // O(N) loop runs N time, constant
         for(int i = 0; i < nums.size(); i++) {
+            // 2
             int value = nums.get(i);
             nums.set(i, 2 * value);
         }
@@ -73,9 +90,12 @@ public class ListOperations {
      * This method modifies the parameter list.
      * @param nums The list of integers
      */
+    // O(2N)
     public void double_iteratorImple(java.util.List<Integer> nums) {
         java.util.ListIterator<Integer> it = nums.listIterator();
+        // O(N) due to sorting
         while(it.hasNext()) {
+            // 2
             int value = it.next();
             it.set(2 * value);
         }
@@ -87,10 +107,15 @@ public class ListOperations {
      * @return True, if at least one pair of duplicate
      * values exists in the list
      */
+
     public boolean hasDupes_getImple(java.util.List<Integer> nums) {
+        // O(3N^2)
         for(int i = 0; i < nums.size(); i++) {
+            // 1
             int value1 = nums.get(i);
+            // O(N^2)
             for(int j = i + 1; j < nums.size(); j++) {
+                // 3
                 int value2 = nums.get(j);
                 if(value1 == value2) {
                     return true;
@@ -106,21 +131,28 @@ public class ListOperations {
      * @return True, if at least one pair of duplicate
      * values exists in the list
      */
+    // O(9N)
     public boolean hasDupes_iteratorImple(java.util.List<Integer> nums) {
         java.util.Iterator<Integer> it1 = nums.iterator();
+        // 1
         int index1 = -1;
+        // O(3N)
         while(it1.hasNext()) {
+            // 3
             index1++;
             int value1 = it1.next();
             java.util.ListIterator<Integer> it2;
             it2 = nums.listIterator(index1 + 1);
+            // O(3N)
             while(it2.hasNext()) {
+                // 3
                 int value2 = it2.next();
                 if(value1 == value2) {
                     return true;
                 }
             }
         }
+        // 1
         return false;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,44 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> nums = new ArrayList<>();
         nums.add(1);
+        nums.add(1);
         nums.add(2);
         nums.add(3);
         nums.add(4);
         nums.add(5);
+
+        List<Integer> nums2 = new LinkedList<>();
+        nums2.add(1);
+        nums2.add(1);
+        nums2.add(2);
+        nums2.add(3);
+        nums2.add(4);
+        nums2.add(5);
 
         ListOperations op = new ListOperations();
 
         int sum = op.sum_getImple(nums);
         System.out.println("Sum: " + sum);
 
+        int sum2 = op.sum_getImple(nums2);
+        System.out.println("Sum: " + sum2);
+
         op.sum_iteratorImple(nums);
         System.out.println("Removed even values: " + nums);
+
+        op.sum_iteratorImple(nums2);
+        System.out.println("Removed even values: " + nums2);
 
         op.double_getImple(nums);
         System.out.println("Doubled values: " + nums);
 
+        op.double_getImple(nums2);
+        System.out.println("Doubled values: " + nums2);
+
         boolean hasDupes = op.hasDupes_getImple(nums);
         System.out.println("Duplicate values: " + hasDupes);
+
+        boolean hasDupes2 = op.hasDupes_getImple(nums2);
+        System.out.println("Duplicate values: " + hasDupes2);
     }
 }
